a18b783d79066bfb057f6d676271eda7d1f6c4eb
Leon Pinto <leonpintoca@gmail.com>
2023-11-21 21:02:04 -0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+import { TodoItem } from "@/components/TodoItem";
+import { prisma } from "@/db";
+import Link from "next/link";
+import { todo } from "node:test";
+
+
+
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.delete({where: {id}})
+}
+
+export default async function Home() {
+
+  const todos = await prisma.todo.findMany()
+
+  return (  
+  <>
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-2x1">Todos</h1>
+      <Link href="/new" className="
+      border border-slate-300 
+      text-slate-300 px-2 py-1 rounded 
+      hover:bg-slate-700 
+      focus-within:bg-slate-700 outline-none"
+      >
+        New
+      </Link>
+    </header>
+    <ul className="p1-4">
+      {todos.map(todo => (
+
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
+        
+      ))}
+    </ul>
+  </>
+  )
+}
